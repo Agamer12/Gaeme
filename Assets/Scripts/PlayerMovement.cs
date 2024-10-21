aca632d53e4531369fe7053fb9b3db2759084184
Joshua Landaverde <landaverdejoshua039@gmail.com>
2024-10-21 01:09:42 -0700

    Implemented movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Objects
+    public GameObject player;
+
+    //Components
+    private Rigidbody2D rb;
+
+    //Variables
+    public float moveSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movement
         
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal"))>0.5)
+        {
+            player.transform.position = new Vector3(player.transform.position.x+Input.GetAxisRaw("Horizontal")*moveSpeed*Time.deltaTime, player.transform.position.y, player.transform.position.z);
+            
+        }
+
+        if(Mathf.Abs(Input.GetAxisRaw("Vertical"))>0.5)
+        {
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y+Input.GetAxisRaw("Vertical")*moveSpeed*Time.deltaTime, player.transform.position.z);
+        }
     }
 }
