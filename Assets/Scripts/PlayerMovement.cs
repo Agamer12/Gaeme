475afaa0b77c3d241090685ad0d5776ae77fdd0f
Joshua Landaverde <landaverdejoshua039@gmail.com>
2024-10-28 14:24:53 -0700

    Setup animator blend trees, made animator parameters change within PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,24 +3,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //Objects
-    private GameObject Player;
+    public GameObject player;
 
     //Components
     private Rigidbody2D rb;
+    private Animator anim;
 
     //Variables
-    [SerializeField] 
-    private float moveSpeed;
-
-    private struct PlayerDirection {
-        int x, y;
-    }
+    public float moveSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Player = gameObject;
+        // Component references
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         
     }
 
@@ -31,22 +28,31 @@ public class PlayerMovement : MonoBehaviour
         
         if(Mathf.Abs(Input.GetAxisRaw("Horizontal"))>0.5)
         {
-            Player.transform.position = new Vector3(Player.transform.position.x+Input.GetAxisRaw("Horizontal")*moveSpeed*Time.deltaTime, Player.transform.position.y, Player.transform.position.z);
-            //TODO changed set player direction
+            player.transform.position = new Vector3(player.transform.position.x+Input.GetAxisRaw("Horizontal")*moveSpeed*Time.deltaTime, player.transform.position.y, player.transform.position.z);
+
+            //animation
+            anim.SetBool("isMoving", true);
+            anim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat("lastMoveY", 0f);
+
         }
 
         if(Mathf.Abs(Input.GetAxisRaw("Vertical"))>0.5)
         {
-            Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y+Input.GetAxisRaw("Vertical")*moveSpeed*Time.deltaTime, Player.transform.position.z);
-            //TODO changed set player direction
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y+Input.GetAxisRaw("Vertical")*moveSpeed*Time.deltaTime, player.transform.position.z);
+
+            //animation
+
+            anim.SetBool("isMoving", true);
+            anim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            anim.SetFloat("lastMoveX", 0);
         }
-    }
 
-    public float getMoveSpeed() {
-        return moveSpeed;
-    }
+        //Animation
 
-    public void setMoveSpeed(float value) {
-        moveSpeed = value;
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal"))<0.5&& Mathf.Abs(Input.GetAxisRaw("Vertical"))<0.5)
+        {
+            anim.SetBool("isMoving", false);
+        }
     }
 }
